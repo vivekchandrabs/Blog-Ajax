6c75a4a363c702b2887e6b2f9f88d9e64bcd8d7b
Vivek Chandra B S <vivek.chandra@speckbit.com>
2019-10-09 18:50:02 +0530

    Add user field to both the models

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField(default=None)
 	timestamp = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
 	def __str__(self):
 		return self.title
@@ -17,6 +19,7 @@ class Comment(models.Model):
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
 	content = models.TextField(default=None)
 	timestamp = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
 	def __str__(self):
 		return f"{self.post} | {self.content}"
