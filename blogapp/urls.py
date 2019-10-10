05a4eb222a543054274b35670f37011544189f5c
Vivek Chandra B S <vivek.chandra@speckbit.com>
2019-10-10 12:36:26 +0530

    Add endpoint to signin and signup view

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from blogapp.views import get_all_posts, make_post, delete_post
+from blogapp.views import get_all_posts, make_post, delete_post, signin, signup
 
 urlpatterns = [
    path("all-posts/", get_all_posts),
@@ -8,5 +8,9 @@ urlpatterns = [
    path("make-post/", make_post),
 
    path("delete-post/<int:post_id>/", delete_post),
+
+   path("signin/",  signin),
+
+   path("signup/", signup),
 
 ]
