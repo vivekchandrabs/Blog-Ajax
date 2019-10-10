5ed4c2977c49c8355e9c8ce7f13ba42c4de4d3eb
Vivek Chandra B S <vivek.chandra@speckbit.com>
2019-10-10 16:15:24 +0530

    Remove webserver urls'

--- blog/urls.py
+++ blog/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include("blogapp.urls")),
+    path('', include("blogapp.urls")),
     path('admin/', admin.site.urls),
-    path('', include("webserver.urls")),
 ]
